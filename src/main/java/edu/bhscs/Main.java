cc62a98bee7b7968f1fabe5ccbccce8281dc1d8c
AshmitaLongia <2027684@apps.nsd.org>
2025-09-12 16:46:35 +0000

    I am am mastering PRINTLN

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,24 @@
-// Student name
-// class period
-// project name
-// date
+// Ashmita Longia
+// P2
+// Zero-Hero
+// 9/12/2025
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: whatever Mr.Reiber says
+ * INPUT: we dont know input yet
+ * OUTPUT: some cool words in the command line terminal thing
+ * EDGE CASE: they are all edge cases at the moment
  */
 
 package edu.bhscs;
+
+class Main{
+
+  public static void main( String[] args){
+    System.out.println("hello");
+    System.out.println("PROGRAM START");
+    System.out.println("-----------");
+    System.out.println("bye");
+
+  }
+}
